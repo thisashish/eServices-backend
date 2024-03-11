5ccc4c13b5c0f81832f4ad69c953676739b2b555
Ashish Vishwakarma <ashish.vishwakarma1267@gmail.com>
2024-03-11 18:26:19 +0530

    Add files via upload

---
+++ models/SPOtp.js
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+
+const SPOtpSchema = new Schema({
+  email: {
+    type: String,
+    require: true,
+  },
+  otp: {
+    type: Number,
+    require: true,
+  },
+});
+
+// user otp model
+const SPOtp = new model("serviceproviderOtps", SPOtpSchema);
+
+export default SPOtp;
---
+++ models/profileSchema.js
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+import validator from "validator";
+
+const profileSchema = new Schema({
+  name:{
+type:String,
+  },
+  img: {
+    type: String,
+    require: false,
+  },
+  
+});
+
+// category otp model
+const Profile = new model("profiles", profileSchema);
+
+// export default Profile;
+
+module.exports = Profile;
